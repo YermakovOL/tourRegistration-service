2c985e549f7d82f1dd4241fffaaab27642254eaf
Olexiy <6diamondsex@gmail.com>
2024-05-26 19:32:51 +0300

    Initial project setup: - Created OpenAPI specification - Configured OpenAPI plugin - Customized OpenAPI generator templates

---
+++ src/main/java/yermakov/oleksii/tourregistrationservice/TourRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package yermakov.oleksii.tourregistrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourRegistrationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourRegistrationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yermakov/oleksii/tourregistrationservice/TourRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package yermakov.oleksii.tourregistrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourRegistrationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
