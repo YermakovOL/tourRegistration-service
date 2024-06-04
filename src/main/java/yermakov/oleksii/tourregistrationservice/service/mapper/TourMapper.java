7b49aa1fd920650d3ae64d277bee7471dca04a9b
Yermakov Oleksiy <@6diamondsex@gmail.com>
2024-06-04 18:52:34 +0300

    Creating TourMapper(#1)

--- src/main/java/yermakov/oleksii/tourregistrationservice/service/mapper/TourMapper.java
+++ src/main/java/yermakov/oleksii/tourregistrationservice/service/mapper/TourMapper.java
@@ -1,7 +1,11 @@
 package yermakov.oleksii.tourregistrationservice.service.mapper;
 
 import org.mapstruct.Mapper;
+import yermakov.oleksii.tourregistrationservice.model.Tour;
+import yermakov.oleksii.tourregistrationservice.model.TourDto;
 
 @Mapper
 public interface TourMapper {
+    TourDto convertTourIntoDto(Tour tour);
+    Tour convertDtoIntoTour(TourDto tourDto);
 }
