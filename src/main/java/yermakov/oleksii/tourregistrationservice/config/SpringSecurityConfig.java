7e03ca0cc5cb420baae42450e499f08b78fecb35
Olexiy <6diamondsex@gmail.com>
2024-06-14 13:13:55 +0300

    Configure OAuth2 resource service

---
+++ src/main/java/yermakov/oleksii/tourregistrationservice/config/SpringSecurityConfig.java
@@ -0,0 +1,22 @@
+package yermakov.oleksii.tourregistrationservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(authorize -> {
+                    authorize.anyRequest().authenticated();
+                })
+                .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> {
+                    httpSecurityOAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults());
+                });
+
+        return http.build();
+    }
+}
