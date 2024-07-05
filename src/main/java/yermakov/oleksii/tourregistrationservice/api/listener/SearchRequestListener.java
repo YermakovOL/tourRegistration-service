38a8e63963a43e4017cffb2db5dfa1f567f48b43
Yermakov Oleksiy <@6diamondsex@gmail.com>
2024-07-05 20:22:26 +0300

    RabbitMq partly configuration

---
+++ src/main/java/yermakov/oleksii/tourregistrationservice/api/listener/SearchRequestListener.java
@@ -0,0 +1,7 @@
+package yermakov.oleksii.tourregistrationservice.api.listener;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+@RabbitListener
+public class SearchRequestListener {
+}
---
+++ src/main/java/yermakov/oleksii/tourregistrationservice/config/RabbitMqConfiguration.java
@@ -0,0 +1,8 @@
+package yermakov.oleksii.tourregistrationservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfiguration {
+    
+}
